5c6054c3916430e0c44ce06314aa575bccc51835
Bilal <taybib4@gmail.com>
2023-01-30 10:13:24 +0100

    I don't know why when I click on a pagination button it does not change, everything stays the same, the selected page doesn't change, and the data does not change as well..

--- app.py
+++ app.py
@@ -4,19 +4,18 @@ import requests
 import json
 
 app = Flask(__name__)
-job_listings = []
-paginationItems = []
 currentPage = 1
 
 @app.route("/")
 def index():
     # scrape()
     return render_template('index.html')
-    # render_template('jobs.html', job_listings=job_listings, paginationItems=paginationItems, currentPage=currentPage)
 
 def flexjobs_scraper(url):
     res = requests.get(url)
     res.raise_for_status()
+    job_listings = []
+    paginationItems  = []
 
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     # nextLink = soup.select('a[rel="next"]')[0].get('href')
@@ -30,62 +29,57 @@ def flexjobs_scraper(url):
             'location': job.select('.job-locations')[0].get_text(),
             'description': job.select('.job-description')[0].get_text()
         }  
-        print(j)
+        # print(j)
         job_listings.append(j)
     
-    # save the result into Json
+    # save the result into Json File
     with open('data.json', 'w') as f:
         json.dump(job_listings, f)
         # next button
         # nextLink = soup.select('a[rel="next"]')[0].get('href')
         # url = 'https://flexjobs.com' + nextLink
         # flexjobs_scraper(url)
-
-def pagination(jobtitle, location):
-    res = requests.get(f'https://www.flexjobs.com/search?search={jobtitle}&location={location}&srt=date')
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    
+    # scrap the pagination link
     pagesnav = soup.select('.page-link')
-
-
+    # get the the first and the last page on the scraped website
     lastnumber = int(pagesnav[len(pagesnav) - 2].get_text())
     firstnumber = int(pagesnav[len(pagesnav) + 1 - len(pagesnav)].get_text())
-
+    # generating a list of pagination items, page number and page link
     for i in range(firstnumber, lastnumber + 1):
-        full_url = f'https://www.flexjobs.com/search?search={jobtitle}&location={location}&srt=date&page={i}'
+        full_url = url + f'&page={i}'
         paginationItems.append({
             'pageNumber':int(i),
             'href': full_url
         })
 
+    return job_listings, paginationItems
+
 
 @app.route('/pagination', methods=['POST'])
 def changepage():
-    currentPage = request.form['currentPage']
-    href = request.form['href']
+    # getting the selected page and link
+    currentPage = int(request.form['currentPage'])
+    href = str(request.form['href'])
+    
+    list = flexjobs_scraper(href)[0]
+    paginationItems = flexjobs_scraper(href)[1]
 
-    print('*' * 30)
-    print(currentPage, href)
-    print('*' * 30)
-    flexjobs_scraper(href)
-    return render_template('jobs.html', job_listings=job_listings, paginationItems=paginationItems, currentPage=currentPage)
+    # return render_template('index.html')
+    return render_template('jobs.html', job_listings=list, paginationItems=paginationItems, currentPage=currentPage)
     # Do something with the page number, like querying a database for data for that page
     
 
 @app.route('/scrape', methods=['POST'])
 def scrape():
-    # jobtitle = request.form['jobtitle']
-    # jobtitle = request.form['jobtitle']
-    jobtitle = 'python'
-    location = 'remote'
-
-    url = f'https://www.flexjobs.com/search?search={jobtitle}&location={location}&srt=date'
+    jobtitle = request.form['jobtitle']
+    location = request.form['location']
+    
+    url = f'https://www.flexjobs.com/search?search={jobtitle}&location={location}&srt=date&page=1'
     try:
-        flexjobs_scraper(url)
-        pagination(jobtitle, location)
+        job_listings = flexjobs_scraper(url)[0]
+        paginationItems = flexjobs_scraper(url)[1]
     except:
         print(len(job_listings))
 
     return render_template('jobs.html', job_listings=job_listings, paginationItems=paginationItems, currentPage=currentPage)
-    
-    
